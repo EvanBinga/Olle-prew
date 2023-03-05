e2ac4b9564851f140a9febda308e821bb8b82846
EvanBinga <106447704+EvanBinga@users.noreply.github.com>
2023-03-05 23:37:18 +0300

    1323
    1323

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,50 +4,15 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function($) {
-	$('.sliderItem').click(function() {
-		$('.popup-fade').fadeIn();
-		return false;
-	});	
-	
-	$('.popup-close').click(function() {
-		$(this).parents('.popup-fade').fadeOut();
-		return false;
-	});		
- 
-	$(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-			e.stopPropagation();
-			$('.popup-fade').fadeOut();
-		}
-	});
-	
-	$('.popup-fade').click(function(e) {
-		if ($(e.target).closest('.popup').length == 0) {
-			$(this).fadeOut();					
-		}
-	});
+
+$(document).ready(function(){
+  $('.sliderItem').click(function(event) {
+    $('.header_popup').toggleClass('active');
+  });
 });
 
-
-
-
-// $(document).ready(function(){
-//   $('.sliderItem').click(function(event) {
-//     $('.header_popup').toggleClass('active');
-//   });
-// });
-
-
-// $(document).ready(function(){
-//   $('.sliderItem').click(function(event) {
-//     $('.shop_popup').toggleClass('active');
-//   });
-// });
-
-// $(document).ready(function(){
-//   $('.clouse').click(function(event) {
-//     $('.shop_popup').removeClass('active');
-//   });
-// });
-
+$(document).ready(function(){
+  $('.header_popup_text_clouse').click(function(event) {
+    $('.header_popup').toggleClass('active');
+  });
+});
